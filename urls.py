2970e59451e8eac8eeac307b4ebf3ffe02870f8e
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2024-08-14 10:50:58 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index,name='index'),
-    path('category',views.category,name='category')
+    path('category',views.category,name='category'),
+    path('viewcategory',views.viewcategory,name='viewcategory'),
+    path('editcategory',views.editcategory,name='editcategory'),
+    path('deletecategory',views.deletecategory,name='deletecategory')
 ]
--- views.py
+++ views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Category
 def index(request):
     return render(request,"adminlte/index.html")
 def category(request):
+    if request.method=="POST":
+        obj = Category(catname=request.POST.get("catname"))
+        obj.save()
+        return render(request,"adminlte/category.html",{"key":"data inserted successfully"})
+
     return render(request,"adminlte/category.html")
+def viewcategory(request):
+    res = Category.objects.all()
+    return render(request,"adminlte/viewcategory.html",{"data":res})
+def editcategory(request):
+     if request.method=="POST":
+        data = Category.objects.get(pk=request.POST.get("txtid"))
+        data.catname = request.POST.get("txtcat")
+        data.save()
+        return redirect('/adminlte/viewcategory')
+     res = Category.objects.get(pk=request.GET.get("q"))
+     return render(request,"adminlte/editcategory.html",{"data":res})
+
+def deletecategory(request):
+    pass
